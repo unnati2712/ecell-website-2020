b473a7f0dc8eb7e156cf8d10cc2328ed23419989
NishantJawla <nishantjawla12225@gmail.com>
2020-10-07 21:57:38 +0530

    Adding background and updating testimonials

---
+++ js/myscript.js
@@ -0,0 +1,53 @@
+document.getElementById('modal1').addEventListener("click", function() {
+    document.querySelector('.bg-modal1').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal1').addEventListener("click", function() {
+	document.querySelector('.bg-modal1').style.display = "none";
+});
+
+document.getElementById('modal2').addEventListener("click", function() {
+    document.querySelector('.bg-modal2').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal2').addEventListener("click", function() {
+	document.querySelector('.bg-modal2').style.display = "none";
+});
+
+document.getElementById('modal3').addEventListener("click", function() {
+    document.querySelector('.bg-modal3').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal3').addEventListener("click", function() {
+	document.querySelector('.bg-modal3').style.display = "none";
+});
+
+document.getElementById('modal4').addEventListener("click", function() {
+    document.querySelector('.bg-modal4').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal4').addEventListener("click", function() {
+	document.querySelector('.bg-modal4').style.display = "none";
+});
+
+document.getElementById('modal5').addEventListener("click", function() {
+    document.querySelector('.bg-modal5').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal5').addEventListener("click", function() {
+	document.querySelector('.bg-modal5').style.display = "none";
+});
+
+document.getElementById('modal6').addEventListener("click", function() {
+    document.querySelector('.bg-modal6').style.display = "flex";
+    console.log("Button was clicked!");
+});
+
+document.getElementById('closemodal6').addEventListener("click", function() {
+	document.querySelector('.bg-modal6').style.display = "none";
+});
